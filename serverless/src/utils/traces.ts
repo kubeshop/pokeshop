c6b523b460f5a4c4c05232e53520f8e8f4f2b21c
Oscar Reyes <oscar-rreyes1@hotmail.com>
2022-11-11 12:50:42 -0600

    serverless initial version

--- serverless/src/utils/traces.ts
+++ serverless/src/utils/traces.ts
@@ -1,10 +1,10 @@
 import * as opentelemetry from '@opentelemetry/api';
 
-export const createSpanFromContext =  async (
+export const createSpanFromContext = async (
   name: string,
   ctx: opentelemetry.Context,
   options?: opentelemetry.SpanOptions | undefined
-): Promise<opentelemetry.Span> {
+): Promise<opentelemetry.Span> => {
   const tracer = await opentelemetry.trace.getTracer('pokeshop-api');
 
   if (!ctx) {
@@ -12,4 +12,4 @@ export const createSpanFromContext =  async (
   }
 
   return tracer.startSpan(name, options, ctx);
-}
+};
