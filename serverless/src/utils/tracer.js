870265fea7c833eaa6c1354a6bcc5d2bfdf8da3d
Oscar Reyes <oscar-rreyes1@hotmail.com>
2022-11-24 17:53:41 -0600

    fixes

--- serverless/src/utils/tracer.js
+++ serverless/src/utils/tracer.js
@@ -1,6 +1,5 @@
 const { SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base');
 const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
-const { AwsLambdaInstrumentation } = require('@opentelemetry/instrumentation-aws-lambda');
 const { registerInstrumentations } = require('@opentelemetry/instrumentation');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
@@ -27,9 +26,10 @@ provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
 
 registerInstrumentations({
   instrumentations: [
-    getNodeAutoInstrumentations(),
-    new AwsLambdaInstrumentation({
-      disableAwsContextPropagation: true,
+    getNodeAutoInstrumentations({
+      '@opentelemetry/instrumentation-aws-lambda': {
+        disableAwsContextPropagation: true,
+      },
     }),
     new PrismaInstrumentation(),
   ],
