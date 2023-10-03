ee0a3234fe912f2f2f93be27fca85b9d0f66041c
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2023-10-03 14:02:23 -0300

    add span event to request validation middleware (#18)

--- api/src/middlewares/validation.ts
+++ api/src/middlewares/validation.ts
@@ -7,12 +7,14 @@ const validate = type => {
   return async function validate(ctx, next) {
     const parentSpan = await getParentSpan();
     const span = await createSpan('validate request', parentSpan, { kind: SpanKind.INTERNAL });
+    span.addEvent("validation started")
 
     const body = ctx.request.body;
     try {
       const validType = await runWithSpan(span, async () => await transformAndValidate(type, body));
       ctx.body = validType;
       span.setAttribute(CustomTags.VALIDATION_IS_VALID, true);
+      span.addEvent("request is valid", {[CustomTags.VALIDATION_IS_VALID]: true})
       return runWithSpan(span, async () => await next(ctx));
     } catch (validationErrors) {
       ctx.status = 400;
@@ -22,6 +24,11 @@ const validate = type => {
         [CustomTags.VALIDATION_ERRORS]: JSON.stringify(response.errors),
         [CustomTags.VALIDATION_IS_VALID]: false,
       });
+      span.addEvent("request is invalid", {
+        [CustomTags.VALIDATION_ERRORS]: JSON.stringify(response.errors),
+        [CustomTags.VALIDATION_IS_VALID]: false,
+      })
+
       span.setStatus({ code: SpanStatusCode.ERROR });
       ctx.body = response;
       return response;
