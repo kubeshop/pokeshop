1e2f9238f361efc502217247afaba5b067461140
Oscar Reyes <oscar-rreyes1@hotmail.com>
2023-12-14 13:57:01 -0600

    upgrading deps

--- cypress/e2e/1-getting-started/home.cy.ts
+++ cypress/e2e/1-getting-started/home.cy.ts
@@ -4,7 +4,38 @@ const TRACETEST_API_TOKEN = Cypress.env('TRACETEST_API_TOKEN') || '';
 
 const tracetest = Tracetest();
 
-const definition = `
+describe('Home', { defaultCommandTimeout: 60000 }, () => {
+  before(done => {
+    tracetest.configure(TRACETEST_API_TOKEN, 'http://localhost:3000').then(() => done());
+  });
+
+  beforeEach(() => {
+    cy.visit('/', {
+      onBeforeLoad: win => tracetest.capture(win.document),
+    });
+  });
+
+  afterEach(done => {
+    const definition = Cypress.env('definition') || '';
+    tracetest.runTest(definition).then(() => done());
+  });
+
+  // uncomment to wait for trace tests to be done
+  after(done => {
+    tracetest.summary().then(() => done());
+  });
+
+  it('create a pokemon', () => {
+    cy.get('[data-cy="create-pokemon-button"]').should('be.visible').click();
+    cy.get('[data-cy="create-pokemon-modal"]').should('be.visible');
+    cy.get('#name').type('Pikachu');
+    cy.get('#type').type('Electric');
+    cy.get('#imageUrl').type('https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-blue-version/8/89/Pikachu.jpg');
+
+    cy.get('button').contains('OK').click();
+  });
+
+  const definition = `
     type: Test
     spec:
       id: aW1wb3J0cyBhIHBva2Vtb24=
@@ -30,38 +61,6 @@ const definition = `
         selector: span[tracetest.span.type="general" name="Tracetest trigger"]
         value: attr:name
     `;
-
-describe('Home', { defaultCommandTimeout: 60000 }, () => {
-  before(done => {
-    tracetest.configure(TRACETEST_API_TOKEN).then(() => done());
-  });
-
-  beforeEach(() => {
-    cy.visit('/', {
-      onBeforeLoad: win => tracetest.capture(win.document),
-    });
-  });
-
-  afterEach(done => {
-    const definition = Cypress.env('definition') || '';
-    tracetest.runTest(definition).then(() => done());
-  });
-
-
-  // uncomment to wait for trace tests to be done
-  after(done => {
-    tracetest.summary().then(() => done());
-  });
-
-  it('create a pokemon', () => {
-    cy.get('[data-cy="create-pokemon-button"]').should('be.visible').click();
-    cy.get('[data-cy="create-pokemon-modal"]').should('be.visible');
-    cy.get('#name').type('Pikachu');
-    cy.get('#type').type('Electric');
-    cy.get('#imageUrl').type('https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-blue-version/8/89/Pikachu.jpg');
-
-    cy.get('button').contains('OK').click();
-  });
 
   it('imports a pokemon', { env: { definition } }, () => {
     cy.get('[data-cy="import-pokemon-button"]').click();
