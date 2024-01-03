328538f3ee6e9cc2d97ff4a2524a402ecf263b3c
xoscar <oscar-rreyes1@hotmail.com>
2024-01-03 12:47:03 -0600

    feat: Playwright E2E Tests

---
+++ playwright/home.spec.ts
@@ -0,0 +1,48 @@
+import { test, expect } from '@playwright/test';
+import { generateTraceParent } from '@tracetest/core';
+
+test.describe.configure({ mode: 'serial' });
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://localhost:8081/');
+  const traceparent = generateTraceParent();
+
+  await page.evaluate(traceparent => {
+    const head = document.querySelector('head');
+    const meta = document.createElement('meta');
+
+    meta.setAttribute('name', 'traceparent');
+    meta.setAttribute('content', traceparent);
+
+    head?.appendChild(meta);
+  }, traceparent);
+});
+
+test('creates a pokemon', async ({ page }) => {
+  expect(await page.getByText('Pokeshop')).toBeTruthy();
+
+  await page.click('text=Add');
+
+  await page.getByLabel('Name').fill('Charizard');
+  await page.getByLabel('Type').fill('Flying');
+  await page
+    .getByLabel('Image URL')
+    .fill('https://upload.wikimedia.org/wikipedia/en/1/1f/Pok%C3%A9mon_Charizard_art.png');
+  await page.getByRole('button', { name: 'OK', exact: true }).click();
+});
+
+test('imports a pokemon', async ({ page }) => {
+  expect(await page.getByText('Pokeshop')).toBeTruthy();
+
+  await page.click('text=Import');
+
+  await page.getByLabel('ID').fill(Math.floor(Math.random() * 101).toString());
+  await page.getByRole('button', { name: 'OK', exact: true }).click();
+});
+
+test('deletes a pokemon', async ({ page }) => {
+  await page.locator('[data-cy="pokemon-list"]');
+
+  await page.locator('[data-cy="pokemon-card"]').first().click();
+  await page.locator('[data-cy="pokemon-card"]').first().locator('[data-cy="delete-pokemon-button"]').click();
+});
