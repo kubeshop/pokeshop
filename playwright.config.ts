c30355cd3047051854c4f02d1c46a682c1c807a6
xoscar <oscar-rreyes1@hotmail.com>
2024-01-12 08:35:15 -0600

    fix(e2e): disabling web security for playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,7 +37,12 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+        launchOptions: {
+          args: ['--disable-web-security'],
+        },
+      },
     },
 
     // {
